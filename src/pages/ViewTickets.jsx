9373d84a03b587b03ccd721158ecbe166620b9b3
Elif Duygu Demircioğlu <95955738+elifedd@users.noreply.github.com>
2023-12-16 19:38:30 +0300

    add flight card styles

--- src/pages/ViewTickets.jsx
+++ src/pages/ViewTickets.jsx
@@ -3,7 +3,6 @@ import ConnectWalletModal from '../components/ConnectWalletModal';
 import Navbar from '../components/Navbar';
 
 function calculateFlightDuration(departureTime, arrivalTime) {
-
   const departure = new Date(`2023-01-01 ${departureTime}`);
   const arrival = new Date(`2023-01-01 ${arrivalTime}`);
   const timeDifference = arrival - departure;
@@ -13,50 +12,62 @@ function calculateFlightDuration(departureTime, arrivalTime) {
   return { hours, minutes };
 }
 
-function Flight({ number, origin, destination, departureTime, arrivalTime }) {
-  
+function Flight({ airline, logo, origin, destination, departureTime, arrivalTime, priceInEth }) {
   const duration = calculateFlightDuration(departureTime, arrivalTime);
+  const [openModal, setOpenModal] = useState(false);
 
   return (
-    <div>
-      <h3>Flight Number: {number}</h3>
-      <p>Origin: {origin}</p>
-      <p>Destination: {destination}</p>
-      <p>Departure Time: {departureTime}</p>
-      <p>Arrival Time: {arrivalTime}</p>
-      <p>Duration: {duration.hours} hours and {duration.minutes} minutes</p>
-      <hr />
+    <div className='flight-card'>
+      <img className='flight-card__logo' src={logo} alt={airline} />
+      <div className='flight-info'>
+        <p className='flight-info__time'>{departureTime}</p>
+        <span className='flight-info__loc'>{origin}</span>
+      </div>
+      <div className='flight-duration'>
+        <svg className='flight-duration__img' xmlns="http://www.w3.org/2000/svg" height="16" width="18" viewBox="0 0 576 512"><path d="M482.3 192c34.2 0 93.7 29 93.7 64c0 36-59.5 64-93.7 64l-116.6 0L265.2 495.9c-5.7 10-16.3 16.1-27.8 16.1l-56.2 0c-10.6 0-18.3-10.2-15.4-20.4l49-171.6L112 320 68.8 377.6c-3 4-7.8 6.4-12.8 6.4l-42 0c-7.8 0-14-6.3-14-14c0-1.3 .2-2.6 .5-3.9L32 256 .5 145.9c-.4-1.3-.5-2.6-.5-3.9c0-7.8 6.3-14 14-14l42 0c5 0 9.8 2.4 12.8 6.4L112 192l102.9 0-49-171.6C162.9 10.2 170.6 0 181.2 0l56.2 0c11.5 0 22.1 6.2 27.8 16.1L365.7 192l116.6 0z"/></svg>
+        <div className='flight-duration__shapes'>
+          <div className='circle'></div>
+          <div className='line'></div>
+          <div className='circle circle-filled'></div>
+        </div>
+        <p className='flight-duration__val'>{duration.hours}h {duration.minutes}m</p>
+      </div>
+      <div className='flight-info'>
+        <p className='flight-info__time'>{arrivalTime}</p>
+        <span className="flight-info__loc">{destination}</span>
+      </div>
+      <div className='flight-action'>
+        <p className='flight-action__price'>{priceInEth} ETH</p>
+        <button
+            className='flight-action__btn'
+            onClick={() => {
+              setOpenModal(true);
+            }}
+          >
+            Buy Flight
+          </button>
+        {openModal && <ConnectWalletModal closeModal={() => setOpenModal(false)} />}
+      </div>
     </div>
   );
 }
 
 function ViewTickets() {
-  const [openModal, setOpenModal] = useState(false);
-  
   const flights = [
-    { number: "FL001", origin: "New York", destination: "Los Angeles", departureTime: "08:00 AM", arrivalTime: "12:00 PM" },
-    { number: "FL002", origin: "Chicago", destination: "Miami", departureTime: "10:30 AM", arrivalTime: "02:45 PM" },
-    { number: "FL003", origin: "San Francisco", destination: "Seattle", departureTime: "01:15 PM", arrivalTime: "03:30 PM" }
-  ];
+    { number: "FL001", airline: "Delta Airlines", logo: "https://s3.amazonaws.com/static.beavercountyradio.com/wp-content/uploads/2020/01/30135727/Delta-Airlines-Logo.jpg", origin: "New York", destination: "Los Angeles", departureTime: "08:00 AM", arrivalTime: "12:00 PM", priceInEth: 2.5 },
+    { number: "FL002", airline: "United Airlines", logo: "https://logowik.com/content/uploads/images/united-airlines-old8802.logowik.com.webp", origin: "Chicago", destination: "Miami", departureTime: "10:30 AM", arrivalTime: "02:45 PM", priceInEth: 1.8 },
+    { number: "FL003", airline: "American Airlines", logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSPtHGeBSudT56w7LzvO74me7EKobaUwQUrS9aokj6Stg&s", origin: "San Francisco", destination: "Seattle", departureTime: "01:15 PM", arrivalTime: "03:30 PM", priceInEth: 3.0 }
+  ];  
 
   return (
-    <div>
-       <Navbar />
-      <div className='fligts-container'>
-        <div className='flight'>
-          {flights.map((flight, index) => (
-            <Flight key={index} {...flight} />
-          ))}
-        </div>
+    <div className='viewTickets'>
+      <Navbar />
+      <div className='flights-container'>
+        {flights.map((flight, index) => (
+          <Flight key={index} {...flight} />
+        ))}
+
       </div>
-      <button
-        onClick={() => {
-          setOpenModal(true);
-        }}
-      >
-        Connect Wallet
-      </button>
-      {openModal && <ConnectWalletModal closeModal={setOpenModal}/>}
     </div>
   );
 }
