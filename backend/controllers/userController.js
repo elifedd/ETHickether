fd4603bf04a2554a9089a8535afce5749eb033db
Seyit KOYUNCU <98276834+SeyitKoyuncu@users.noreply.github.com>
2023-12-16 21:27:19 +0300

    Added creating txt file when clicking the button

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,9 +4,15 @@ const deneme = async (req, res) => {
       // Handle the POST request
       const { key1, key2 } = req.body;
       console.log(req.body);
-  
-      // Process the data and send a response
-      const responseData = { message: `Here is the key, key1: ${key1}, key2: ${key2}` };
+      // Create file for sending flight information txt via ipfs
+      const fs = require('fs');
+      fs.appendFile('FlightsInformation.txt', 'Will come to flights informations in here' + key1 + key2, (err) =>
+      {
+        if(err) throw err;
+        console.log('File Created');
+      });
+
+      const responseData = { message: `File creaation is succesfull. Here is the keys: ${key1}, key2: ${key2}` };
       res.json(responseData);
     } catch (error) {
       console.error(error);
