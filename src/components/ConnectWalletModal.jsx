a36b7e7eae1452cf7ae3cbccc1ecc10be207b439
Elif Duygu Demircioğlu <95955738+elifedd@users.noreply.github.com>
2023-12-16 23:11:36 +0300

    add disconnect modal feature and connected wallet state

--- src/components/ConnectWalletModal.jsx
+++ src/components/ConnectWalletModal.jsx
@@ -6,6 +6,7 @@ import axios from 'axios';
 
 function ConnectWalletModal({ closeModal }) {
   const [isWalletConnected, setWalletConnected] = useState(false);
+  const [walletAddress, setWalletAddress] = useState(null);
 
   const handleButtonClick = async () => {
     connect();
@@ -23,28 +24,41 @@ function ConnectWalletModal({ closeModal }) {
     }
   };
 
-async function connect() {
-  if (window.ethereum) {
-    try {
-      // Request access to the user's accounts
-      const accounts = await window.ethereum.request({ method: "eth_requestAccounts" });
+  async function connect() {
+    if (window.ethereum) {
+      try {
+        // Request access to the user's accounts
+        const accounts = await window.ethereum.request({ method: "eth_requestAccounts" });
 
-      // Use the current MetaMask selected/active wallet address
-      const walletAddress = accounts.length > 0 ? accounts[0] : undefined;
+        // Use the current MetaMask selected/active wallet address
+        const walletAddress = accounts.length > 0 ? accounts[0] : undefined;
 
-      if (walletAddress) {
-        setWalletConnected(true);
-        console.log(`Wallet: ${walletAddress}`);
-      } else {
-        console.error('No wallet address available.');
+        if (walletAddress) {
+          setWalletConnected(true);
+          setWalletAddress(walletAddress);
+          console.log(`Wallet: ${walletAddress}`);
+        } else {
+          console.error('No wallet address available.');
+        }
+      } catch (error) {
+        console.error('Error connecting wallet:', error);
       }
-    } catch (error) {
-      console.error('Error connecting wallet:', error);
+    } else {
+      console.log("No wallet");
     }
-  } else {
-    console.log("No wallet");
   }
-}
+
+  async function disconnect() {
+    try {
+      // Clear the wallet connection state
+      setWalletConnected(false);
+      setWalletAddress(null);
+  
+      console.log('Wallet disconnected');
+    } catch (error) {
+      console.error('Error disconnecting wallet:', error);
+    }
+  }
 
   return (
     <div className='modalBackground'>
@@ -55,15 +69,34 @@ async function connect() {
           <CloseIcon />
         </button >
         <div className='modalContainer-left'>
-          <h2 className='modalContainer-left__title'>Connect your wallet</h2>
-          <p className='modalContainer-left__subtitle'>Select the network that you want to connect below</p>
-          <div className='modalContainer-left__container'>
-            <h3 className='modalContainer-left__container--title'>Choose Wallet</h3>
-            <button className='modalContainer-left__container--btn' onClick={handleButtonClick}>
-              <MetaMask />
-              <span>MetaMask</span>
-            </button>
-          </div>
+          {isWalletConnected ? ( 
+            <div className='wallet-card-container'>
+                <div className='wallet-card'>
+                  <div className="wallet">
+                    <h2 className="wallet-header">Your Wallet</h2>
+                    <div className="wallet-account">
+                      <div className="wallet-account__img"></div>
+                      <div className="wallet-account__text">
+                        <p>{walletAddress}</p>
+                        <span>Sepholia Account</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <button className='disconnect-btn' onClick={disconnect}>disconnect wallet</button>
+              </div>
+              
+            ) : (
+              <>
+                <h2 className='modalContainer-left__title'>Connect your wallet</h2><p className='modalContainer-left__subtitle'>Select the network that you want to connect below</p><div className='modalContainer-left__container'>
+                <h3 className='modalContainer-left__container--title'>Choose Wallet</h3>
+                <button className='modalContainer-left__container--btn' onClick={handleButtonClick}>
+                  <MetaMask />
+                  <span>MetaMask</span>
+                </button>
+                </div>
+              </>
+          )}
         </div>
         <div className='modalContainer-right'>
           <DigitalWalletPayment />
